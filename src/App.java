bd277ce79b538258a862f44a8fc1aa0ee04703d1
Azinisikhona Siyalana <azizsiyalana10@gmail.com>
2024-06-05 20:11:46 +0200

    still can't get it to validate

---
+++ src/App.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class App {
+
+    int validPin = 2002, enteredPin, attempts = 1, delaySecs = 5;
+    boolean isValidInput;
+    Scanner input = new Scanner(System.in);
+
+    public void validateInput() {
+        while (!isValidInput) {
+            isValidInput = false;
+            System.out.println("Please enter your pin: ");
+            if (input.hasNextInt()) {
+                enteredPin = input.nextInt();
+                isValidInput = true;
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                input.next();
+            }
+        }
+    }
+
+    public void trial() {
+        validateInput();
+
+        while (enteredPin != validPin) {
+            if (attempts <= 2) {
+                System.out.println("You have entered an invalid pin!!!");
+                System.out.print("Please try again: ");
+                enteredPin = input.nextInt();
+            } else if (attempts == 3) {
+                try {
+                    System.out.println("Incorrect pin!!! 2 attempts left");
+                    System.out.println("Please wait " + delaySecs + " seconds");
+                    Thread.sleep(delaySecs * 1000);
+                    System.out.print("Please try again: ");
+                    enteredPin = input.nextInt();
+                } catch (InterruptedException e) {
+                    System.out.println("An error has occured " + e);
+                }
+            } else if (attempts == 4) {
+                try {
+                    System.out.println("Incorrect pin!!! 1 attempts left");
+                    System.out.println("Please wait " + delaySecs * 2 + " seconds");
+                    Thread.sleep(delaySecs * 2000);
+                    System.out.print("Please try again: ");
+                    enteredPin = input.nextInt();
+                } catch (InterruptedException e) {
+                    System.out.println("An error has occured " + e);
+                }
+            } else {
+                System.out.println("You have inputed a wrong pin too many times, your account has been suspended");
+                break;
+            }
+            attempts++;
+        }
+        if (enteredPin == validPin) {
+            System.out.println("Welcome to your account");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Enter pin: ");
+        App log = new App();
+        log.trial();
+    }
+}

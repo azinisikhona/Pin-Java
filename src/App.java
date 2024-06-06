c4d54fe0025da4dfb64393d65a826e3af3947a6d
Azinisikhona Siyalana <130047240+azinisikhona@users.noreply.github.com>
2024-06-06 10:44:23 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -60,7 +60,6 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        // System.out.println("Enter pin: ");
         App log = new App();
         log.trial();
     }
